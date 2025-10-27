f0f497a244384924439ac595de90c2b78f43873e
Omette <ometteomega@gmail.com>
2025-10-27 05:16:51 -0500

    feat: getComputerChoice and getUserChoice
    Made functions to get the choices from both players. Also made two
    functions for winning/losing.

---
+++ script.js
@@ -0,0 +1,37 @@
+const { use } = require("react");
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3);
+    switch(randomNum){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function win(userChoice, computerChoice){
+    humanScore++;
+    console.log(`You won! ${userChoice} beats ${computerChoice}`);
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    console.log(`You lost! ${computerChoice} beats ${userChoice}`);
+}
+
+function getUserChoice(){
+    let choice = prompt("Rock, Paper, or Scissors?");
+    return choice.toLowerCase();
+}
+
+
+
+//MAIN
+
+let humanScore,
+    computerScore 
+    = 0;
+
